44d6e80bca48a2ea2220074450f3231b7d31b674
ErikaNunex <erikanunes0512@gmail.com>
2022-09-29 09:40:16 -0300

    resolvendo exercicio

--- ibge/eventos.js
+++ ibge/eventos.js
@@ -3,8 +3,32 @@ fetch('https://servicodados.ibge.gov.br/api/v1/localidades/regioes')
     .then((regioes) => {
         regioes.forEach( (cadaRegiao) => {
             document.getElementById('regiao').innerHTML+=`
-            <option>${cadaRegiao.nome}</option>`;
+            <option value="${cadaRegiao.id}">${cadaRegiao.nome}</option>`;
     });
 });
 
+function pegarEstado(){
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${regiao.value}/estados`)
+    .then((resposta) => resposta.json())
+    .then((estados) => {
+        estado.innerHTML = '';
+        estados.forEach( (cadaEstado) => {
+            document.getElementById('estado').innerHTML+= `
+            <option value="${cadaEstado.id}">${cadaEstado.nome}</option>`;
+    });
+});
+}
 
+function pegarCidade(){
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estado.value}/municipios`)
+    .then((resposta) => resposta.json())
+    .then((cidades) => {
+        cidade.innerHTML = '';
+        cidades.forEach( (cadaCidade) => {
+            document.getElementById('cidade').innerHTML+= `
+            <option>${cadaCidade.nome}</option>`;
+    });
+});
+}
+
+ 
